2a0d5b296d1f5a3434f8bf9fb22453ec9d16b2f3
zleda9 <zleda9@naver.com>
2020-07-15 12:15:46 +0900

    초기백업

---
+++ hello.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main() {
+    printf("Hello, world!");
+    printf("안녕하세요!");
+    return 0;    
+}
---
+++ hello2.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    int lang = 0;
+    if (lang == 1)
+        printk("Hello, world!");
+    else 
+        printf("안녕하세요!");
+    return 0;    
+}
