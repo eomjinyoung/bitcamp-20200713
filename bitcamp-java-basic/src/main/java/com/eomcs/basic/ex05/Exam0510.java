0ba5c362dc61276fef96d5392e217bea1fe543de
eomjinyoung <jinyoung.eom@gmail.com>
2020-07-29 17:15:57 +0900

    .

--- bitcamp-java-basic/src/main/java/com/eomcs/basic/ex05/Exam0510.java
+++ bitcamp-java-basic/src/main/java/com/eomcs/basic/ex05/Exam0510.java
@@ -8,18 +8,17 @@ public class Exam0510 {
     // => 조건 ? 표현식1 : 표현식2
     // => 조건이 참이면 표현식1을 실행하고,
     //    조건이 거짓이면 표현식2를 실행한다.
-    int age = 20;
-    
-    /* 
-    (age > 18) ? System.out.println("성인이다.") : 
-                     System.out.println("미성년자이다.");  //컴파일 오류!
-    */
-    // 표현식 자리에는 문자가 되었든 숫자가 되었든 
+    int age = 11;
+
+    //    (age > 18) ? System.out.println("성인이다.") :
+    //                     System.out.println("미성년자이다.");  //컴파일 오류!
+
+    // 표현식 자리에는 문자가 되었든 숫자가 되었든
     // 실행 결과가 놓여져야 한다.
     // 위의 System.out.println(...) 문장은 결과를 리턴하지 않는다.
     // 그래서 컴파일 오류이다.
 
-    String message = (age > 18) ? "성년" : "미성년";
+    String message = age > 18 ? "성년" : "미성년";
     System.out.printf("나이 %d는(은) %s이다.\n", age, message);
   }
 }
