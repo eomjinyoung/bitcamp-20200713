1a956c213f9213ef45f8cf2909669c36397b9677
eomjinyoung <jinyoung.eom@gmail.com>
2020-07-30 16:21:53 +0900

    .

---
+++ bitcamp-java-basic/src/main/java/com/eomcs/basic/ex06/Exam0310.java
@@ -0,0 +1,13 @@
+// 반복문: while (조건) 문장;  do 문장 while(조건);
+package com.eomcs.basic.ex06;
+
+public class Exam0310 {
+  public static void main(String[] args) {
+    int i = 0;
+
+    do {
+      System.out.println(i);
+      i++;
+    } while (i < 10);
+  }
+}
