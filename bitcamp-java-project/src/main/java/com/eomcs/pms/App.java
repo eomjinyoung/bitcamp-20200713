c2d6c762b74365048dcdf8e235b3caf6d876260d
eomjinyoung <jinyoung.eom@gmail.com>
2020-07-31 14:51:30 +0900

    .

--- bitcamp-java-project/src/main/java/com/eomcs/pms/App.java
+++ bitcamp-java-project/src/main/java/com/eomcs/pms/App.java
@@ -2,6 +2,7 @@ package com.eomcs.pms;
 
 // 클래스의 패키지 정보를 미리 선언하여 컴파일러에게 알려준다.
 import java.sql.Date;
+import java.util.Scanner;
 
 // 1) 회원의 번호, 이름, 이메일, 암호, 사진, 전화번호, 등록일을 담을 수 있는
 //    메모리를 설계한다. => Member 클래스 정의
@@ -23,27 +24,19 @@ public class App {
       Date createdDate;
     }
 
-    // Member 설계도에 따라 메모리 생성
-    new Member();
+    // Member 인스턴스의 주소를 담을 레퍼런스 배열을 준비한다.
+    final int LENGTH = 100;
+    Member[] members = new Member[LENGTH]; // [ , , , , ]
 
-    /*
+
     Scanner keyInput = new Scanner(System.in);
 
     System.out.println("[회원]");
 
-    final int MAX_LENGTH = 5;
-    int[] no = new int[MAX_LENGTH];
-    String[] name = new String[MAX_LENGTH];
-    String[] email = new String[MAX_LENGTH];
-    String[] password = new String[MAX_LENGTH];
-    String[] photo = new String[MAX_LENGTH];
-    String[] tel = new String[MAX_LENGTH];
-    Date[] now = new Date[MAX_LENGTH];
-
     long currentMillis = 0;
     int count = 0;
 
-    for (int i = 0; i < MAX_LENGTH; i++) {
+    for (int i = 0; i < LENGTH; i++) {
       count++;
       System.out.print("번호? ");
       no[i] = keyInput.nextInt();
@@ -79,7 +72,7 @@ public class App {
     keyInput.close();
 
     System.out.println("---------------------------");
-
+    /*
     for (int i = 0; i < count; i++) {
       System.out.printf("%d, %s, %s, %s, %s\n",
           no[i], name[i], email[i], tel[i], now[i].toString());
