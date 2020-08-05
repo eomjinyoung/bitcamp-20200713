47880bef0e9bce99341764d99a9481a6dfef2242
eomjinyoung <jinyoung.eom@gmail.com>
2020-08-05 15:29:24 +0900

    .

--- bitcamp-java-basic/src/main/java/com/eomcs/oop/ex02/Exam0210.java
+++ bitcamp-java-basic/src/main/java/com/eomcs/oop/ex02/Exam0210.java
@@ -5,30 +5,20 @@ import com.eomcs.util.Calculator;
 public class Exam0210 {
 
   public static void main(String[] args) {
-    // 연산자 우선순위를 고려하지 말고 나온 순서대로 계산하라!
-    // 식1) 2 + 3 - 1 * 7 / 3 = 9
-    // 식2) 3 * 2 + 7 / 4 - 5 = ?
-
-    // 식1과 식2를 동시에 계산하려면 result 변수를 두 개 준비해야 한다.
+    // 2 + 3 - 1 * 7 / 3 = 9
     Calculator c1 = new Calculator();
     Calculator c2 = new Calculator();
 
-    // 현재 버전에서는 식1과 식2를 차례대로 계산해야 한다.
-    c1.plus(2); // 식1) 0 + 2
-    c2.plus(3); // 식2) 0 + 3
-    c1.plus(3); // 식1) 0 + 2 + 3
-    c2.multiple(2); // 식2) 0 + 3 * 2
-    c1.minus(1); // 식1) 0 + 2 + 3 - 1
-    c2.plus(7); // 식2) 0 + 3 * 2 + 7
-    c1.multiple(7); // 식1) 0 + 2 + 3 - 1 * 7
-    c2.minus(5); // 식2) 0 + 3 * 2 + 7 / 4 - 5
-    c1.divide(3); // 식1) 0 + 2 + 3 - 1 * 7 / 3
-    c2.divide(4); // 식2) 0 + 3 * 2 + 7 / 4
+    c1.plus(2);
+    c1.plus(3);
+    c1.minus(1);
+    c1.multiple(7);
+    c1.divide(3);
 
     printResult(c1.result);
-    printResult(c2.result);
-
   }
+
+
 
   static void printResult(int value) {
     System.out.println("**********************");
--- bitcamp-java-basic/src/main/java/com/eomcs/util/Calculator.java
+++ bitcamp-java-basic/src/main/java/com/eomcs/util/Calculator.java
@@ -2,15 +2,8 @@ package com.eomcs.util;
 
 public class Calculator {
 
-  // result 변수는 각 계산 마다 개별적으로 관리해야 한다.
-  // => non-static 변수로 선언하라!
-  // => non-static 변수는 new 연산자를 통해서만 만들 수 있다.
-  // => non-static 변수는 Heap 영역에 생성된다.
-  //
   public int result = 0;
 
-  // non-static 메서드(인스턴스 메서드)는
-  // 인스턴스 주소를 받는 this 라는 빌트인 변수가 있다
   public void plus(int value) {
     this.result += value;
   }
