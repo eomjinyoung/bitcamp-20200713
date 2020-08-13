c8e9b8f9f172808462ee8d524df81233210a547e
eomjinyoung <jinyoung.eom@gmail.com>
2020-08-13 17:44:29 +0900

    .

---
+++ bitcamp-java-basic/src/main/java/com/eomcs/Test01.java
@@ -0,0 +1,10 @@
+package com.eomcs;
+
+import bitcamp.java.Score;
+
+public class Test01 {
+  public static void main(String[] args) {
+    Score s1 = new Score("hong", 100, 100, 100);
+    s1.print();
+  }
+}
