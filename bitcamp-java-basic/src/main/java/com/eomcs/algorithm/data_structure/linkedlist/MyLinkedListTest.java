7b9252d7ad291bcbbd0b0eb2288c1169252763f1
eomjinyoung <jinyoung.eom@gmail.com>
2020-08-17 12:27:02 +0900

    .

---
+++ bitcamp-java-basic/src/main/java/com/eomcs/algorithm/data_structure/linkedlist/MyLinkedListTest.java
@@ -0,0 +1,12 @@
+package com.eomcs.algorithm.data_structure.linkedlist;
+
+public class MyLinkedListTest {
+  public static void main(String[] args) {
+    MyLinkedList list = new MyLinkedList();
+
+    list.add("aaa");
+    list.add("bbb");
+    list.add("ccc");
+    list.add("ddd");
+  }
+}
