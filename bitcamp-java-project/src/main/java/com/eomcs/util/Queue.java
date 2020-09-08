c3b1328e251cbb14c40defede560f3af156e77e8
eomjinyoung <jinyoung.eom@gmail.com>
2020-09-08 14:41:29 +0900

    .

--- bitcamp-java-project/src/main/java/com/eomcs/util/Queue.java
+++ bitcamp-java-project/src/main/java/com/eomcs/util/Queue.java
@@ -42,40 +42,30 @@ public class Queue<E> extends LinkedList<E> {
   // Queue 자료 구조에 맞춰 오버라이딩 한다.
   @Override
   public Iterator<E> iterator() {
-    // QueueIterator 생성자에서 clone()에서 발생한 예외를 처리하도록 프로그래밍 했기 때문에
-    // 여기서는 할 필요가 없다.
+    class QueueIterator implements Iterator<E> {
+      Queue<E> queue;
+
+      public QueueIterator() {
+        try {
+          this.queue = Queue.this.clone();
+        } catch (Exception e) {
+          throw new RuntimeException("큐를 복제하는 중에 오류 발생!");
+        }
+      }
+
+      @Override
+      public boolean hasNext() {
+        return queue.size() > 0;
+      }
+
+      @Override
+      public E next() {
+        if (queue.size() == 0)
+          throw new NoSuchElementException();
+        return queue.poll();
+      }
+    }
     return new QueueIterator();
   }
 
-  // 논스태틱 중첩 클래스로 정의한 Iterator 구현체 
-  // => 바깥 클래스의 인스턴스 멤버를 직접 접근할 수 있다.
-  // => 생성자에서 바깥 클래스의 인스턴스를 받을 필요가 없다.
-  // => 바깥 클래스의 타입 파라미터를 그대로 사용하면 된다.
-  private class QueueIterator implements Iterator<E> {
-    Queue<E> queue;
-
-    public QueueIterator() {
-      try {
-        // 큐는 한 번 poll() 하면 데이터가 제거된다.
-        // 따라서 복제본을 만들어 사용한다.
-        this.queue = Queue.this.clone();
-      } catch (Exception e) {
-        // 큐를 복제할 때 오류가 발생한다면,
-        // 이 메서드를 호출한 쪽에 실행 오류를 던진다.
-        throw new RuntimeException("큐를 복제하는 중에 오류 발생!");
-      }
-    }
-
-    @Override
-    public boolean hasNext() {
-      return queue.size() > 0;
-    }
-
-    @Override
-    public E next() {
-      if (queue.size() == 0)
-        throw new NoSuchElementException();
-      return queue.poll();
-    }
-  }
 }
--- bitcamp-java-project/src/main/java/com/eomcs/util/Stack.java
+++ bitcamp-java-project/src/main/java/com/eomcs/util/Stack.java
@@ -43,40 +43,31 @@ public class Stack<E> extends LinkedList<E> {
   // Stack 자료 구조에 맞춰 오버라이딩 한다.
   @Override
   public Iterator<E> iterator() {
-    // StackIterator의 생성자에서 clone()에서 발생한 예외를 처리하도록 프로그래밍 했기 때문에
-    // 여기서는 할 필요가 없다.
+    class StackIterator implements Iterator<E> {
+      Stack<E> stack;
+
+      public StackIterator() {
+        try {
+          this.stack = Stack.this.clone();
+        } catch (Exception e) {
+          throw new RuntimeException("큐를 복제하는 중에 오류 발생!");
+        }
+      }
+
+      @Override
+      public boolean hasNext() {
+        return !stack.empty();
+      }
+
+      @Override
+      public E next() {
+        if (stack.empty())
+          throw new NoSuchElementException();
+        return stack.pop();
+      }
+    }
     return new StackIterator();
   }
 
-  // 논스태틱 중첩 클래스로 정의한 Iterator 구현체 
-  // => 바깥 클래스의 인스턴스 멤버를 직접 접근할 수 있다.
-  // => 생성자에서 바깥 클래스의 인스턴스를 받을 필요가 없다.
-  // => 바깥 클래스의 타입 파라미터를 그대로 사용하면 된다.
-  private class StackIterator implements Iterator<E> {
-    Stack<E> stack;
 
-    public StackIterator() {
-      try {
-        // 스택은 한 번 pop() 하면 데이터가 제거된다.
-        // 따라서 복제본을 만들어 사용한다.
-        this.stack = Stack.this.clone();
-      } catch (Exception e) {
-        // 스택을 복제할 때 오류가 발생한다면,
-        // 이 메서드를 호출한 쪽에 실행 오류를 던진다.
-        throw new RuntimeException("큐를 복제하는 중에 오류 발생!");
-      }
-    }
-
-    @Override
-    public boolean hasNext() {
-      return !stack.empty();
-    }
-
-    @Override
-    public E next() {
-      if (stack.empty())
-        throw new NoSuchElementException();
-      return stack.pop();
-    }
-  }
 }
