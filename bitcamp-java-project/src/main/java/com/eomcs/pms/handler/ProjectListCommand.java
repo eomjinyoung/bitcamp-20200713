ae5000cf71b89924c6813c705b074a6e612113d5
eomjinyoung <jinyoung.eom@gmail.com>
2020-09-11 18:03:51 +0900

    .

---
+++ bitcamp-java-project/src/main/java/com/eomcs/pms/handler/ProjectListCommand.java
@@ -0,0 +1,36 @@
+package com.eomcs.pms.handler;
+
+import java.util.Iterator;
+import java.util.List;
+import com.eomcs.pms.domain.Project;
+
+public class ProjectListCommand implements Command {
+
+  List<Project> projectList;
+
+  public ProjectListCommand(List<Project> list) {
+    this.projectList = list;
+  }
+
+
+  @Override
+  public void execute() {
+    System.out.println("[프로젝트 목록]");
+
+    // 전체 목록을 조회할 때 `Iterator` 객체를 사용한다.
+    // 만약 목록의 일부만 조회하면다면 인덱스를 직접 다루는 이전 방식을 사용해야 한다.
+    Iterator<Project> iterator = projectList.iterator();
+
+    while (iterator.hasNext()) {
+      Project project = iterator.next();
+      System.out.printf("%d, %s, %s, %s, %s, [%s]\n",
+          project.getNo(),
+          project.getTitle(),
+          project.getStartDate(),
+          project.getEndDate(),
+          project.getOwner(),
+          project.getMembers());
+    }
+  }
+
+}
