d5080b06190052ec2ef7840a1b57151b8c395e97
eomjinyoung <jinyoung.eom@gmail.com>
2020-09-15 18:06:02 +0900

    .

--- bitcamp-java-basic/src/main/java/com/eomcs/io/ex01/Exam0610.java
+++ bitcamp-java-basic/src/main/java/com/eomcs/io/ex01/Exam0610.java
@@ -7,15 +7,20 @@ public class Exam0610 {
 
   public static void main(String[] args) throws Exception {
 
-    File dir = new File(".");
-    File[] files = dir.listFiles(new FileFilter() {
+    class JavaFilter implements FileFilter {
       @Override
       public boolean accept(File file) {
         if (file.isFile() && file.getName().endsWith(".java"))
           return true; // 조회 결과에 포함시켜라!
         return false; // 조회 결과에서 제외하라!
       }
-    });
+    }
+
+    File dir = new File(".");
+
+    JavaFilter javaFilter = new JavaFilter();
+
+    File[] files = dir.listFiles(javaFilter);
 
     for (File file : files) {
       System.out.printf("%s %12d %s\n", 
