4447a2cb947e300e692ef9b0324ccac0bcfa4218
eomjinyoung <jinyoung.eom@gmail.com>
2020-09-17 10:11:23 +0900

    .

--- bitcamp-java-basic/src/main/java/com/eomcs/io/ex01/Exam0610.java
+++ bitcamp-java-basic/src/main/java/com/eomcs/io/ex01/Exam0610.java
@@ -1,34 +1,21 @@
 package com.eomcs.io.ex01;
 
 import java.io.File;
-import java.io.FileFilter;
 
 public class Exam0610 {
 
   public static void main(String[] args) throws Exception {
 
-    class JavaFilter implements FileFilter {
-      @Override
-      public boolean accept(File file) {
-        if (file.isFile() && file.getName().endsWith(".java"))
-          return true; // 조회 결과에 포함시켜라!
-        return false; // 조회 결과에서 제외하라!
-      }
+    File file = new File(".");
+
+    File[] files = file.listFiles(f -> f.isFile() && f.getName().endsWith(".java"));
+
+    for (File f : files) {
+      System.out.printf("%s %10s %s\n", 
+          f.isDirectory() ? "d" : "-",
+              f.length() > 0 ? f.length() : "",
+                  f.getName());
     }
-
-    File dir = new File(".");
-
-    JavaFilter javaFilter = new JavaFilter();
-
-    File[] files = dir.listFiles(javaFilter);
-
-    for (File file : files) {
-      System.out.printf("%s %12d %s\n", 
-          file.isDirectory() ? "d" : "-", 
-              file.length(),
-              file.getName());
-    }
-
   }
 
 }
--- bitcamp-java-basic/src/main/java/com/eomcs/oop/ex12/Exam0310.java
+++ bitcamp-java-basic/src/main/java/com/eomcs/oop/ex12/Exam0310.java
@@ -1,26 +1,16 @@
 // 아규먼트에 람다(lambda) 활용
 package com.eomcs.oop.ex12;
-
 public class Exam0310 {
-
   static interface Player {
     void play();
   }
-
   static void testPlayer(Player p) {
     p.play();
   }
-
   public static void main(String[] args) {
-    // 로컬 클래스
-    class MyPlayer implements Player {
-      @Override
-      public void play() {
-        System.out.println("실행!");
-      }
-    }
-
-    testPlayer(new MyPlayer());
+    // 람다
+   
+    testPlayer(() -> System.out.println("실행....."));
 
   }
 }
