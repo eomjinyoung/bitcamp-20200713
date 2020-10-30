0c0c7dc20779a7e397732e3316e47109dc6b6856
eomjinyoung <jinyoung.eom@gmail.com>
2020-10-30 17:29:49 +0900

    .

--- bitcamp-java-project-server/README.md
+++ bitcamp-java-project-server/README.md
@@ -1,102 +1,27 @@
-# 34-g. 네트워크 API를 활용하여 C/S 구조로 전환하기 :  PMS 코드를 C/S로 분리
+# 36-b. 스레드풀을 이용하여 스레드를 재사용하기 : 자바에서 제공하는 스레드풀 사용하기
 
 이번 훈련에서는,
-- **자바 네트워크 API** 를 사용하여 클라이언트/서버 통신 애플리케이션을 만든다. 
+- 자바의 **Concurrent 프레임워크** 에서 제공하는 스레드풀을 프로젝트에 적용해 볼 것이다.
+
+**Concurrent 프레임워크** 는,
+- 동시성 프로그래밍을 위해 자바에서 제공하는 프레임워크이다.
+- 스레드풀이나 비동기 입출력, 간단한 태스크 프레임워크를 포함하고 있다.
+
 
 ## 훈련 목표
-- Gradle 빌드 도구를 이용하여 자바 애플리케이션 프로젝트를 만드는 것을 연습한다.
-- Gradle 빌드 도구를 이용하여 Eclipse IDE 용 프로젝트로 전환하는 것을 연습한다.
+- 자바에서 제공하는 **Concurrent 프레임워크** 의 **스레드풀** 사용법을 연습한다.
 
 ## 훈련 내용
-- 자바 프로젝트로 사용할 폴더를 만들고 Gradle 빌드 도구로 초기화시킨다.
-- Gradle eclipse 플러그인을 사용하여 Eclipse IDE 용 설정 파일을 준비한다.
-
+- 자바에서 제공하는 **스레드풀** 을 사용하여 클라이언트 요청을 다룬다.
 
 ## 실습
 
-### 1단계 - JSON 데이터 포맷을 다룰 Gson 라이브러리를 추가한다.
+### 1단계 - 기존의 `ThreadPool` 대신에 `ExecutorService` 를 사용한다.  
 
-- **build.gradle** 파일에 gson 라이브러리 정보를 추가한다.
-- `$ gradle eclipse` 를 실행하여 라이브러리를 프로젝트에 추가한다.
-- 이클립스 IDE에서 프로젝트를 `refresh` 한다.
+- `com.eomcs.pms.ServerApp` 변경
+  - `shutdown()` 이후에 모든 스레드의 종료 여부를 확인한다.
+  - 아직 종료 안된 스레드가 있으면 강제 종료시킨다.
 
-### 2단계 - 기존 애플리케이션에서 관련된 패키지 및 클래스를 가져온다.
-
-- com.eomcs.pms.domain 패키지 및 그 하위 클래스를 가져온다.
-- com.eomcs.pms.handler 패키지 및 그 하위 클래스를 가져온다.
-- com.eomcs.context 패키지 및 그 하위 클래스를 가져온다.
-- com.eomcs.pms.listener 패키지 및 그 하위 클래스를 가져온다.
-- com.eomcs.pms.App 클래스에서 옵저버 패턴과 관련된 코드를 가져온다.
-
-#### 작업 파일
-- com.eomcs.pms.domain.* 추가
-- com.eomcs.pms.handler.* 추가
-
-
-### 2단계 - `ClientHandler` 를 `ServerApp` 의 중첩 클래스로 만든다.
-
-- `ServerApp` 클래스 변경
-  - `ClientHandler` 클래스를 중첩 클래스(static nested class)로 만든다.
-
-#### 작업 파일
-- com.eomcs.pms.ServerApp 변경
-  - 백업: ServerApp02.java
-
-### 3단계 - `ClientHandler` 를 `ServerApp.main()` 의 익명 클래스로 만든다.
-
-- `ServerApp` 클래스 변경
-  - `ClientHandler` 스태틱 중첩 클래스를 main()의 익명 클래스로 만든다.
-
-#### 작업 파일
-- com.eomcs.pms.ServerApp 변경
-  - 백업: ServerApp03.java
-
-### 4단계 - `ClientHandler` 를 `ServerApp.main()` 의 익명 클래스로 만든다. II
-
-- `ServerApp` 클래스 변경
-  - 익명 클래스의 코드를 밖깥 클래스의 멤버로 만들어 놓고 사용한다.
-  - 왜? 코드를 읽기 쉽도록 하기 위함이다.
-  - 코드가 여러 블록에 중접되면 될 수록 들여쓰기 하면서 
-    코드를 읽기가 불편해진다.
-
-#### 작업 파일
-- com.eomcs.pms.ServerApp 변경
-  - 백업: ServerApp04.java
-
-### 5단계 - 익명 클래스를 람다(lambda) 문법으로 정의한다.
-
-- `ServerApp` 클래스 변경
-  - 익명 클래스가 메서드 한 개짜리 인터페이스를 구현하고,
-    그 코드도 간단하다면, 
-    람다 문법으로 표현하는 것이 편하다.
-
-#### 작업 파일
-- com.eomcs.pms.ServerApp 변경
-  
 ## 실습 결과
-- src/main/java/com/eomcs/pms/ClientHandler.java 추가
+- src/main/java/com/eomcs/util/concurrent/ThreadPool.java 삭제
 - src/main/java/com/eomcs/pms/ServerApp.java 변경
-
-
-
-
-
-
-## 학습목표
-
-- 클라이언트의 연결을 기다리고 데이터 통신을 수행할 수 있다.
-
-## 실습 소스 및 결과
-
-- src/main/java/com/eomcs/lms/ServerApp.java 변경
-
-## 실습  
-
-### 훈련 1: 클라이언트 연결을 대기하라.
-
-- ServerApp.java 변경
-
-### 훈련 2: 클라이언트가 보낸 메시지를 읽고 응답 메시지를 전송하라.
-
-- ServerApp.java 변경
-
--- bitcamp-java-project-server/src/main/java/com/eomcs/pms/ServerApp.java
+++ bitcamp-java-project-server/src/main/java/com/eomcs/pms/ServerApp.java
@@ -10,12 +10,26 @@ import java.util.ArrayList;
 import java.util.Hashtable;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 import com.eomcs.context.ApplicationContextListener;
+import com.eomcs.pms.handler.Command;
+import com.eomcs.pms.listener.AppInitListener;
+import com.eomcs.pms.listener.DataHandlerListener;
+import com.eomcs.pms.listener.RequestMappingListener;
 
 public class ServerApp {
 
+  // 클라이언트가 "stop" 명령을 보내면 이 값이 true로 변경된다.
+  // - 이 값이 true 이면 다음 클라이언트 접속할 때 서버를 종료한다.
+  static boolean stop = false;
+
+  // 스레드풀 준비
+  ExecutorService threadPool = Executors.newCachedThreadPool();
+
   // 옵저버와 공유할 맵 객체
-  Map<String,Object> context = new Hashtable<>();
+  static Map<String,Object> context = new Hashtable<>();
 
   // 옵저버를 보관할 컬렉션 객체
   List<ApplicationContextListener> listeners = new ArrayList<>();
@@ -45,22 +59,62 @@ public class ServerApp {
   }
 
   public void service(int port) {
+
+    notifyApplicationContextListenerOnServiceStarted();
+
     try (ServerSocket serverSocket = new ServerSocket(port)) {
       System.out.println("서버 실행 중...");
 
       while (true) {
         Socket clientSocket = serverSocket.accept();
-        // 람다 문법 사용
-        new Thread(() -> handleClient(clientSocket)).start();
+
+        if (stop) {
+          break;
+        }
+        // 직접 스레드를 생성하는 것이 아니라 스레드풀에 작업을 맡긴다.
+        threadPool.execute(() -> handleClient(clientSocket));
       }
 
     } catch (Exception e) {
       e.printStackTrace();
     }
+
+    notifyApplicationContextListenerOnServiceStopped();
+
+    // 스레드풀을 종료한다.
+    threadPool.shutdown();
+
+    try {
+      // 스레드풀의 모든 스레드가 종료될 때까지 기다린다.
+      if (!threadPool.awaitTermination(10, TimeUnit.SECONDS)) {
+        System.out.println("아직 종료 안된 작업이 있다.");
+        System.out.println("남아 있는 작업의 강제 종료를 시도하겠다.");
+        // => 만약 10초가 경과될 때까지 종료되지 않으면,
+        //    수행 중인 작업을 강제 종료하라고 지시하고,
+        //    대기 중인 작업은 취소한다.
+        threadPool.shutdownNow();
+
+        // 그리고 다시 작업이 종료될 때까지 기다린다.
+        if (!threadPool.awaitTermination(5, TimeUnit.SECONDS)) {
+          System.out.println("스레드풀의 강제 종료를 완료하지 못했다.");
+        } else {
+          System.out.println("모든 작업을 강제 종료했다.");
+        }
+      }
+    } catch (Exception e) {
+      System.out.println("스레드풀 종료 중 오류 발생!");
+    }
+    System.out.println("서버 종료!");
   }
 
   public static void main(String[] args) {
     ServerApp server = new ServerApp();
+
+    // 리스너(옵저버/구독자) 등록
+    server.addApplicationContextListener(new AppInitListener());
+    server.addApplicationContextListener(new DataHandlerListener());
+    server.addApplicationContextListener(new RequestMappingListener());
+
     server.service(8888);
   }
 
@@ -73,12 +127,27 @@ public class ServerApp {
         BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
         PrintWriter out = new PrintWriter(socket.getOutputStream())) {
 
-      while (true) {
-        String request = in.readLine();
-        sendResponse(out, request);
-        if (request.equalsIgnoreCase("quit"))
-          break;
+      // 클라이언트가 보낸 요청을 읽는다.
+      String request = in.readLine();
+
+      if (request.equalsIgnoreCase("stop")) {
+        stop = true; // 서버의 상태를 멈추라는 의미로 true로 설정한다.
+        out.println("서버를 종료하는 중입니다!");
+        out.println();
+        out.flush();
+        return;
       }
+
+      Command command = (Command) context.get(request);
+      if (command != null) {
+        command.execute(out, in);
+      } else {
+        out.println("해당 명령을 처리할 수 없습니다!");
+      }
+
+      // 응답의 끝을 알리는 빈 문자열을 보낸다.
+      out.println();
+      out.flush();
 
     } catch (Exception e) {
       System.out.println("클라이언트와의 통신 오류!");
@@ -86,11 +155,5 @@ public class ServerApp {
 
     System.out.printf("클라이언트(%s)와의 연결을 끊었습니다.\n",
         address.getHostAddress());
-  }
-
-  private static void sendResponse(PrintWriter out, String message) {
-    out.println(message);
-    out.println(); // 응답이 끝났음을 알리는 빈 줄을 보낸다.
-    out.flush();
   }
 }
